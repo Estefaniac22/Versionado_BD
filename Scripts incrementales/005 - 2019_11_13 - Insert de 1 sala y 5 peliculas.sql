14e57c11835f205633fc66b224b3af1d6308ade8
Estefania <estefaniac.25@hotmail.com>
2019-11-13 19:02:11 -0300

    Subida de script 005 - Insert de sala y pelicula

---
+++ Scripts incrementales/005 - 2019_11_13 - Insert de 1 sala y 5 peliculas.sql
@@ -0,0 +1,20 @@
+USE Cine;
+INSERT INTO Scripts
+VALUES(5, 005, "Insert sala_peliculas", '2019_11_13');
+
+INSERT INTO Salas
+VALUES(6, "Cine Tadicor", 7);
+
+INSERT INTO Autores
+VALUES( 21,"Woody Allen",21),
+( 22,"J.M. Barrie", 22),
+( 23,"Christopher Hampton", 23),
+( 24, "Lulu Wang", 24),
+( 25,"John Lasseter", 25);
+
+INSERT INTO Peliculas
+VALUES(21, "Medianoche en París", 13,2008,21),
+(22,"Finding Neverland", 3, 2002, 22),
+(23, "Vidas al límite", 18,1995, 23),
+(24, "The farewell", 12, 2019, 24),
+(25, "Toy Story 4", 3, 2019, 25);
