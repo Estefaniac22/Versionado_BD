9aecf363a5983b70e0aec11d1e6ef78d43c659db
Estefania <estefaniac.25@hotmail.com>
2019-11-13 18:46:03 -0300

    subida de script 003 - Agregado campo año

---
+++ 003 - 2019_11_13 - Agregado de campo Año a Peliculas.sql
@@ -0,0 +1,26 @@
+INSERT INTO Scripts
+VALUES(3, 003, "Agregado de Año", '2019_11_13');
+
+ALTER TABLE Peliculas
+ADD Año INT;
+
+UPDATE Peliculas SET Año = 1993 WHERE Codigo =1;
+UPDATE Peliculas SET Año = 1998 WHERE Codigo =2;
+UPDATE Peliculas SET Año = 2007 WHERE Codigo =3;
+UPDATE Peliculas SET Año = 2005 WHERE Codigo =4;
+UPDATE Peliculas SET Año = 1959 WHERE Codigo =5;
+UPDATE Peliculas SET Año = 2005 WHERE Codigo =6;
+UPDATE Peliculas SET Año = 2001 WHERE Codigo =7;
+UPDATE Peliculas SET Año = 2004 WHERE Codigo =8;
+UPDATE Peliculas SET Año = 1997 WHERE Codigo =9;
+UPDATE Peliculas SET Año = 2003 WHERE Codigo =10;
+UPDATE Peliculas SET Año = 1945 WHERE Codigo =11;
+UPDATE Peliculas SET Año = 1997 WHERE Codigo =12;
+UPDATE Peliculas SET Año = 2002 WHERE Codigo =13;
+UPDATE Peliculas SET Año = 1988 WHERE Codigo =14;
+UPDATE Peliculas SET Año = 2001 WHERE Codigo =15;
+UPDATE Peliculas SET Año = 1959 WHERE Codigo =16;
+UPDATE Peliculas SET Año = 1996 WHERE Codigo =17;
+UPDATE Peliculas SET Año = 1985 WHERE Codigo =18;
+UPDATE Peliculas SET Año = 2007 WHERE Codigo =19;
+UPDATE Peliculas SET Año = 2003 WHERE Codigo =20;
