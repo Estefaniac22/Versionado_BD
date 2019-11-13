40bae624c474ca50e8e4589fa70dc26d29da18e6
Estefania <estefaniac.25@hotmail.com>
2019-11-13 18:41:08 -0300

    subida de script 002 - Primer Insert

---
+++ 002 - 2019_11_13 - Insert de 5 salas y 20 peliculas.sql
@@ -0,0 +1,32 @@
+USE Cine;
+INSERT INTO Scripts
+VALUES(2, 002, "Insert de peliculas", '2019_11_13');
+
+INSERT INTO Salas
+VALUES(1, "Village",2),
+(2,"Cine Universidad", 5),
+(3,"Cinemacenter", 3),
+(4,"Microcine Municipal",1),
+(5,"Cinemark",10);
+
+INSERT INTO Peliculas
+VALUES(1, "Tierra de penumbra", 16),
+(2,"Shakespeare enamorado", 18),
+(3,"La joven Jan Austen", 16),
+(4, "El secreto de los hermanos Grimm", 6),
+(5, "Dias sin vida", 13),
+(6, "Truman Capote", 6),
+(7, "Harry Potter", 3),
+(8, "Descubriendo Nunca Jamás", 13),
+(9, "Un cuento de hadas", 3),
+(10, "Soldados de salamina", 6),
+(11, "Cancion inolvidable", 13),
+(12, "Wilde", 6),
+(13, "Las horas", 16),
+(14, "Remando al viento", 13),
+(15, "Iris", 13),
+(16, "El diario de Ana frank", 13),
+(17, "Hamsun", 6),
+(18, "Memorias de África", 13),
+(19, "La escafandra y la mariposa", 3),
+(20, "Sylvia", 6);
